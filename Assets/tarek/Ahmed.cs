4333eaeccf346f1ca4e4dae5925e3e396e584ed5
AhmedYousef <ahmed_200099250@yahoo.com>
2019-04-11 14:40:07 +0200

    firist commit

---
+++ Assets/tarek/Ahmed.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ahmed : MonoBehaviour {
+	public GameObject mesh;
+	public Renderer ren;
+	public Material[] mat;
+	public Texture tex;
+	public Color hexcolor;
+	public float r;
+	private static Ahmed Soundinstance;
+	public static Ahmed SoundInstance 
+	{
+		get
+		{
+			return Soundinstance;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		Soundinstance=this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+ 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+		
+        if (Physics.Raycast(ray, out hit, 1000))
+		{
+			Debug.DrawLine(ray.origin, hit.point);
+			ren = mesh.GetComponent<Renderer>();
+			mat = ren.materials;
+			//r=hexcolor.r;
+			//mat[1].color =Color.red;
+			mat[1].mainTexture=tex;
+			
+		}
+            
+        
+	}	
+}
---
+++ Assets/tarek/Select.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour {
+	public GameObject a;
+	//public GameObject canves;
+	// Use this for initialization
+	void Start () {
+		//a.GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown()
+	{
+		//canves.SetActive(true);
+		Ahmed.SoundInstance.mesh=a;
+	}
+}
